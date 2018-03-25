c3d61dbe3a6fb2ee29589e757964d4989a793b5d
Daniel Earwicker <dan@earwicker.com>
2018-03-25 19:50:54 +0100

    Auto deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pushd MirrorBall.Server
+echo Building...
 dotnet publish -f netcoreapp2.0 -c Release
 popd
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 git add .
 git commit -m 'Auto deploy'
 git push
+echo studio...
 ssh 192.168.1.10 'cd ~/mirrorball && git pull && . build.sh'
+echo telly...
 ssh 192.168.1.11 'cd ~/mirrorball && git pull && . build.sh'
