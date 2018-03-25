e79621a222193ed3b1f68b3109001ec74f9e1708
Daniel Earwicker <dan@earwicker.com>
2018-03-25 20:29:40 +0100

    Auto deploy

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ pushd MirrorBall.Server
 echo Building...
 dotnet publish -f netcoreapp2.0 -c Release
 popd
+pushd MirrorBall.Client
+npm install
+npx webpack
+popd
 launchctl stop com.earwicker.mirrorball
