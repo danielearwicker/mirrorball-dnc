eda19e75a6980bf9ad468d1c590e414183896e58
Daniel Earwicker <dan@earwicker.com>
2018-03-25 19:54:07 +0100

    Auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 git add .
 git commit -m 'Auto deploy'
 git push
+buildcmd = bash --login -c 'cd ~/mirrorball && git pull && . build.sh';
 echo studio...
-ssh 192.168.1.10 'cd ~/mirrorball && git pull && . build.sh'
+ssh 192.168.1.10 "${buildcmd}"
 echo telly...
-ssh 192.168.1.11 'cd ~/mirrorball && git pull && . build.sh'
+ssh 192.168.1.11 "${buildcmd}"
